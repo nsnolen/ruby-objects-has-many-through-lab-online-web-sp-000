c25c312092d6d6f9c2019a3c7d1cb8a983a9b015
Sara Nolen <nolensara1@gmail.com>
2021-02-18 18:59:22 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,14 @@
+class Appointment
+  attr_reader :patient, :doctor
+  attr_accessor :date
+
+  @@all = []
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+
+end
