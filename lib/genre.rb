c2d4733dda6dd919f57100d01c044835277c77ba
Sara Nolen <nolensara1@gmail.com>
2021-02-18 19:20:46 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,15 +1,15 @@
-class Genre 
-  attr_reader :name 
+class Genre
+  attr_reader :name
   @@all = []
 
   def initialize(name)
     @name = name
-    @@all << self 
+    @@all << self
   end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
 
 end
